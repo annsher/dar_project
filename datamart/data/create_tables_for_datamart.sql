fdca285404bbcd5af235c8f400af0d83e15b708d
Anya <49087113+annsher@users.noreply.github.com>
2023-07-29 22:14:19 +0300

    Add files via upload

--- datamart/data/create_tables_for_datamart.sql
+++ datamart/data/create_tables_for_datamart.sql
@@ -4,13 +4,18 @@ CREATE TABLE IF NOT EXISTS datamart.avg_purchase (
 	recorded_on date NULL,
 	pos_name varchar(255) NULL,
 	revenue_daily numeric NULL,
-	avg_amount numeric NULL
+	avg_amount numeric NULL,
+	CONSTRAINT avg_purchase_fk FOREIGN KEY (pos_name) REFERENCES datamart.pos(pos_name)
 );
 
 CREATE TABLE IF NOT EXISTS datamart.avg_purchase_month (
 	"month" varchar(255) NULL,
 	avg_amount_month numeric NULL,
-	pos_name varchar(255) NULL
+	pos_name varchar(255) NULL,
+	"year" int4 NULL,
+	CONSTRAINT avg_purchase_month_fk FOREIGN KEY (pos_name) REFERENCES datamart.pos(pos_name),
+	CONSTRAINT avg_purchase_month_fk_1 FOREIGN KEY ("year") REFERENCES datamart.years("year"),
+	CONSTRAINT avg_purchase_month_fk_2 FOREIGN KEY ("month") REFERENCES datamart.months("month")
 );
 
 CREATE TABLE IF NOT EXISTS datamart.revenue_month (
@@ -18,21 +23,32 @@ CREATE TABLE IF NOT EXISTS datamart.revenue_month (
 	"year" int4 NULL,
 	category_name varchar(255) NULL,
 	revenue numeric NULL,
-	pos_name varchar(255) NULL
+	pos_name varchar(255) NULL,
+	CONSTRAINT revenue_month_fk FOREIGN KEY (pos_name) REFERENCES datamart.pos(pos_name),
+	CONSTRAINT revenue_month_fk_1 FOREIGN KEY (category_name) REFERENCES datamart.category(category_name),
+	CONSTRAINT revenue_month_fk_2 FOREIGN KEY ("month") REFERENCES datamart.months("month"),
+	CONSTRAINT revenue_month_fk_3 FOREIGN KEY ("year") REFERENCES datamart.years("year")
 );
 
 CREATE TABLE IF NOT EXISTS datamart.sold_item (
 	recorded_on date NULL,
 	qnt int4 NULL,
 	pos_name varchar(255) NULL,
-	category_name varchar(255) NULL
+	category_name varchar(255) NULL,
+	CONSTRAINT sold_item_fk FOREIGN KEY (pos_name) REFERENCES datamart.pos(pos_name),
+	CONSTRAINT sold_item_fk_1 FOREIGN KEY (category_name) REFERENCES datamart.category(category_name)
 );
 
 CREATE TABLE IF NOT EXISTS datamart.sold_item_month (
 	"month" varchar(255) NULL,
 	qnt_month int4 NULL,
 	pos_name varchar(255) NULL,
-	category_name varchar(255) NULL
+	category_name varchar(255) NULL,
+	"year" int4 NULL,
+	CONSTRAINT sold_item_month_fk FOREIGN KEY (pos_name) REFERENCES datamart.pos(pos_name),
+	CONSTRAINT sold_item_month_fk_1 FOREIGN KEY (category_name) REFERENCES datamart.category(category_name),
+	CONSTRAINT sold_item_month_fk_2 FOREIGN KEY ("month") REFERENCES datamart.months("month"),
+	CONSTRAINT sold_item_month_fk_3 FOREIGN KEY ("year") REFERENCES datamart.years("year")
 );
 
 CREATE TABLE IF NOT EXISTS datamart.to_order (
@@ -50,6 +66,35 @@ CREATE TABLE IF NOT EXISTS datamart.top_5 (
 	"month" varchar(255) NULL,
 	"year" int4 NULL,
 	qnt_sum int4 NULL,
-	pos_name varchar(255) NULL
+	pos_name varchar(255) NULL,
+	CONSTRAINT top_5_fk FOREIGN KEY (pos_name) REFERENCES datamart.pos(pos_name),
+	CONSTRAINT top_5_fk_1 FOREIGN KEY (category_name) REFERENCES datamart.category(category_name),
+	CONSTRAINT top_5_fk_2 FOREIGN KEY ("month") REFERENCES datamart.months("month"),
+	CONSTRAINT top_5_fk_3 FOREIGN KEY ("year") REFERENCES datamart.years("year")
 );
 
+CREATE TABLE IF NOT EXISTS datamart.pos (
+	pos varchar(255) NULL,
+	pos_name varchar(255) NOT NULL,
+	CONSTRAINT pos_pk PRIMARY KEY (pos_name)
+);
+
+CREATE TABLE IF NOT EXISTS datamart.category (
+	category_name varchar(255) NOT NULL,
+	category_id varchar(255) NULL,
+	CONSTRAINT category_pk PRIMARY KEY (category_name)
+);
+
+CREATE TABLE IF NOT EXISTS datamart.updated_on (
+	"date" date NULL
+);
+
+CREATE TABLE IF NOT EXISTS datamart.months (
+	"month" varchar NOT NULL,
+	CONSTRAINT months_pk PRIMARY KEY (month)
+);
+
+CREATE TABLE IF NOT EXISTS datamart.years (
+	"year" int4 NOT NULL,
+	CONSTRAINT years_pk PRIMARY KEY (year)
+);
--- datamart/data/datamart.py
+++ datamart/data/datamart.py
@@ -13,8 +13,19 @@ def create_table_intern(conn_i):
     f = open('create_tables_for_datamart.sql', 'r')
     create = ''.join(f.readlines())
     cursor.execute(create)
-    for tables in ['avg_purchase', 'avg_purchase_month', 'revenue_month', 'sold_item', 'sold_item_month', 'to_order', 'top_5']:
+    for tables in ['avg_purchase', 'avg_purchase_month', 'revenue_month', 'sold_item', 'sold_item_month', 'to_order', 'top_5', 'pos', 'category', 'updated_on', 'months', 'years']:
         truncate_table_intern(f'{tables}', conn_i.raw_connection())
+    pos = pd.read_sql_table('pos', conn_i.connect(), 'DDS', coerce_float=False)
+    pos.to_sql('pos', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
+    category = pd.read_sql_table('category', conn_i.connect(), 'DDS', coerce_float=False)
+    category.to_sql('category', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
+    months = pd.DataFrame(['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'], columns = ['month'])
+    months.to_sql('months', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
+    records = []
+    for i in range(2020, datetime.datetime.now().year + 1):
+        records.append(i)
+    years = pd.DataFrame(records, columns=['year'])
+    years.to_sql('years', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
     conn_i.raw_connection().commit()
     cursor.close()
     print('Таблицы загружены')
@@ -51,10 +62,11 @@ def fill_avg_purchase_month(conn_i):
     merged = transaction.merge(merged, how='left', on='transaction_id')
     merged.insert(loc=len(merged.columns), column='total', value=merged['quantity']*merged['price'])
     merged.insert(loc=len(merged.columns), column='month', value=merged['recorded_on'].dt.month)
+    merged.insert(loc=len(merged.columns), column='year', value=merged['recorded_on'].dt.year)
     merged['month'] = merged['month'].astype(str)
     merged['month'] = merged['month'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'], ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'])
-    merged = merged.groupby(['month', 'pos_name'], as_index=False)['total'].agg(['mean'])
-    merged.columns = ['month', 'pos_name', 'avg_amount_month']
+    merged = merged.groupby(['month', 'year', 'pos_name'], as_index=False)['total'].agg(['mean'])
+    merged.columns = ['month', 'year', 'pos_name', 'avg_amount_month']
     merged.to_sql('avg_purchase_month', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
     print('avg_purchase_month uploaded')
 
@@ -106,12 +118,13 @@ def fill_sold_item_month(conn_i):
     merged = merged.merge(transaction_pos, how='left', on='transaction_id')
     merged = merged.merge(pos, how='left', on='pos')
     merged.insert(loc=len(merged.columns), column='month', value=merged['recorded_on'].dt.month)
+    merged.insert(loc=len(merged.columns), column='year', value=merged['recorded_on'].dt.year)
     merged['month'] = merged['month'].astype(str)
     merged['month'] = merged['month'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
                                               ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август',
                                                'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'])
-    merged = merged.groupby(['month', 'category_name', 'pos_name'], as_index=False)['quantity'].agg(['sum'])
-    merged.columns = ['month', 'category_name', 'pos_name', 'qnt_month']
+    merged = merged.groupby(['month', 'year', 'category_name', 'pos_name'], as_index=False)['quantity'].agg(['sum'])
+    merged.columns = ['month', 'year', 'category_name', 'pos_name', 'qnt_month']
     merged.to_sql('sold_item_month', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
     print('sold_item_month uploaded')
 
@@ -157,6 +170,11 @@ def fill_to_order(conn_i):
     result.to_sql('to_order', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
     print('to_order uploaded')
 
+def fill_updated_on(conn_i):
+    upd = pd.DataFrame([datetime.datetime.now()], columns =['date'])
+    upd.to_sql('updated_on', conn_i.connect(), 'datamart', 'append', index=False, method='multi')
+    print('updated_on uploaded')
+
 from sqlalchemy import create_engine
 
 conn_i = create_engine("postgresql+psycopg2://interns_4:dlf6y?@10.1.108.29:5432/internship_4_db")
@@ -168,3 +186,4 @@ fill_sold_item(conn_i)
 fill_sold_item_month(conn_i)
 fill_top_5(conn_i)
 fill_to_order(conn_i)
+fill_updated_on(conn_i)
